6030697a4a588f6c108cc55b016a1a5e757988b1
Teguh Tri Nugroho <ttrinugroho@gmail.com>
2021-02-17 18:08:57 +0700

    carousel fix

--- js/settingcarousel.js
+++ js/settingcarousel.js
@@ -36,3 +36,25 @@ $('#small-carousel').owlCarousel({
     }
   }
 })
+
+$(document).ready(function () {
+  $(".tautan-carousel").owlCarousel();
+});
+$('.tautan-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  navText: ['<span><i class="bx bxs-chevron-left" ></i></span>',
+    '<span><i class="bx bxs-chevron-right"></i></span>'
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true
+    },
+    768: {
+      items: 3,
+      nav: true
+    }
+  }
+})
